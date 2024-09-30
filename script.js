343441f8b7b315daed1487f029a74671cb7892b1
Nael-Basset <nael.basset1@gmail.com>
2024-09-30 23:21:48 +0200

    fff

--- script.js
+++ script.js
@@ -5,15 +5,7 @@ function toggleMenu() {
 
 function toggleTheme() {
     const body = document.body;
-    const themeButton = document.querySelector('.toggle-theme');
-
     body.classList.toggle('theme-dark');
     body.classList.toggle('theme-light');
-
-    // Changer le texte du bouton
-    if (body.classList.contains('theme-dark')) {
-        themeButton.textContent = "Mode Clair";
-    } else {
-        themeButton.textContent = "Mode Sombre";
-    }
 }
+
